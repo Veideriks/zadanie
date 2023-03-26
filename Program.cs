82a16f181198f639a15c5d2e8d34e9e4294fe1af
Veider <a.l.e.x.1.9@yandex.ru>
2023-03-26 14:09:59 +0300

    Задан метод для распечатки массива

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿string[] stringArray = new string[6];
+
+void PrintArray (string [] stringArray)
+{
+    for (int i = 0; i<stringArray.Length; i++)
+        Console.Write($"{stringArray[i]} ");
+}
+
+PrintArray (stringArray);
