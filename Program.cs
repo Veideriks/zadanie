f57ef351f3c46ac2c714b846e679c6a200b8063b
Veider <a.l.e.x.1.9@yandex.ru>
2023-03-26 14:37:40 +0300

    Сделал второй массив и решил задачу

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
-﻿string[] stringArray = new string[6];
+﻿string[] stringArray = { "123", "sdasf", "12", "safsavbx", "--9", "njncxvo" };
+string[] stringArray2 = {"", "", "", "", "", ""};
 
-void PrintArray (string [] stringArray)
+void PrintArray(string[] stringArray)
 {
-    for (int i = 0; i<stringArray.Length; i++)
+    for (int i = 0; i < stringArray.Length; i++)
         Console.Write($"{stringArray[i]} ");
 }
 
-PrintArray (stringArray);
+PrintArray(stringArray);
+Console.WriteLine();
+
+string str = "null";
+for (int i = 0; i < stringArray.Length; i++)
+    {
+        str = stringArray[i];
+        if (str.Length <= 3)
+            stringArray2[i]=stringArray[i];
+    }
+PrintArray(stringArray2);
+Console.WriteLine();
